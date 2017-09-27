6c69daa7408f28bb93ad0f03d178d5cda6c573b9
CreamYong <32336184+CreamYong@users.noreply.github.com>
2017-09-28 00:58:07 +0900

    Add files via upload

---
+++ Assets/edge_info.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class edge_info {
+
+	public float dist;
+	public int v1;
+	public int v2;
+	
+	public edge_info(int i1, int i2, Vector3 v31, Vector3 v32) {
+		v1 = i1;
+		v2 = i2;
+		dist = Mathf.Sqrt ( (v31.x - v32.x)*(v31.x-v32.x) + (v31.y-v32.y)*(v31.y-v32.y));
+	}
+	
+	
+}
